e055b99dff7fcaacce71667e4e4400aac7cde663
Maryna-Zakrevska <lovemama.com.ua@gmail.com>
2021-11-02 23:00:00 +0200

    HW 8 - n1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+
+let timerId = null;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startButton.addEventListener('click', () => {timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor(); startButton.setAttribute("disabled", true)}, 1000)});
+
+
+stopButton.addEventListener("click", () => {clearInterval(timerId); startButton.removeAttribute("disabled")});
 
