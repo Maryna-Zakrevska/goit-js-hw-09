e3a5859c0217ad4d273db1fa79aa75e52a8ca047
Maryna-Zakrevska <lovemama.com.ua@gmail.com>
2021-11-08 02:14:29 +0200

    HW9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,18 +1,23 @@
-const startButton = document.querySelector('button[data-start]');
-const stopButton = document.querySelector('button[data-stop]');
-const bodyRef = document.querySelector('body');
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+const bodyRef = document.querySelector("body");
 
 let timerId = null;
+stopButton.setAttribute("disabled", true);
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-startButton.addEventListener('click', () => {timerId = setInterval(() => {
-    bodyRef.style.backgroundColor = getRandomHexColor(); startButton.setAttribute("disabled", true)}, 1000)});
+startButton.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    startButton.setAttribute("disabled", true);
+    stopButton.removeAttribute("disabled");
+  }, 1000);
+});
 
-
-stopButton.addEventListener("click", () => {clearInterval(timerId); startButton.removeAttribute("disabled")});
-
-
-
-
+stopButton.addEventListener("click", () => {
+  clearInterval(timerId);
+  startButton.removeAttribute("disabled");
+  stopButton.setAttribute("disabled", true);
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const buttonRef = document.querySelector("[type='submit']");
+const formRef = document.querySelector(".form");
+const delayInputRef = formRef.querySelector("[name='delay']");
+const stepInputRef = formRef.querySelector("[name='step']");
+const amountInputRef = formRef.querySelector("[name='amount']");
+
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = event.target;
+  const delayValue = Number(formData.delay.value);
+  const stepValue = Number(formData.step.value);
+  const amountValue = Number(formData.amount.value);
+
+  for (let position = 0; position < amountValue; position += 1) {
+    const delay = delayValue + stepValue * position;
+    createPromise({ position, delay })
+      .then(({ position, delay } = {}) => {
+        Notify.success(`Fulfilled promise ${position+1} in ${delay}ms`);
+      })
+      .catch(({ position, delay } = {}) => {
+        Notify.failure(`Rejected promise ${position+1} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise({ position, delay }) {
+  const shouldResolve = Math.random() > 0.3;
+  const promiseResult = (resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  };
+
+  const promise = new Promise(promiseResult);
+  return promise;
 }
